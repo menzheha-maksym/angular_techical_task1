f102df086ded4eae379278f9cf6cd116b4c1368b
haswell <haswell@company.com>
2021-10-19 12:43:33 +0300

    add image to card

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
@@ -11,28 +9,9 @@ import { environment } from 'src/environments/environment';
 })
 
 export class DashboardComponent implements OnInit {
-  /** Based on the screen size, switch from standard to one column per row */
-  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-  //   map(({ matches }) => {
-  //     if (matches) {
-  //       return [
-  //         { title: 'Card 1', cols: 1, rows: 1 },
-  //         { title: 'Card 2', cols: 1, rows: 1 },
-  //         { title: 'Card 3', cols: 1, rows: 1 },
-  //         { title: 'Card 4', cols: 1, rows: 1 }
-  //       ];
-  //     }
 
-  //     return [
-  //       { title: 'Card 1', cols: 2, rows: 1 },
-  //       { title: 'Card 2', cols: 1, rows: 1 },
-  //       { title: 'Card 3', cols: 1, rows: 2 },
-  //       { title: 'Card 4', cols: 1, rows: 1 }
-  //     ];
-  //   })
-  // );
 
-  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getData();
@@ -42,6 +21,8 @@ export class DashboardComponent implements OnInit {
 
 
   cards: Card[] = [];
+
+  imgTest = "https://www.pikpng.com/pngl/b/522-5228401_of-front-end-development-was-using-javascript-and.png";
 
   getData() {
     this.http.get<any>(`https://newsapi.org/v2/everything?q=Apple&from=2021-10-18&sortBy=popularity&apiKey=${this.apiKey}`)
@@ -56,6 +37,6 @@ export class DashboardComponent implements OnInit {
 
 type Card = {
   title: string,
-  imgUrl: string
+  urlToImage: string
 }
 
